5d6f79e284455f5745b017864adeeb36d3cc21be
Moderocky <admin@moderocky.com>
2023-02-07 11:24:03 +0000

    Add basic tests for commands.

--- core/src/test/java/mx/kenzie/centurion/CommandTest.java
+++ core/src/test/java/mx/kenzie/centurion/CommandTest.java
@@ -3,13 +3,18 @@ package mx.kenzie.centurion;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Objects;
+
+import static mx.kenzie.centurion.Arguments.INTEGER;
+import static mx.kenzie.centurion.Arguments.STRING_END;
 
 public class CommandTest extends Command<TestSender> {
 
     @BeforeClass
     public static void setup() {
         assert new CommandTest().behaviour != null; // warm up
+        assert new CommandTest().execute(null, "") != null; // warm up
     }
 
     @Test
@@ -31,6 +36,24 @@ public class CommandTest extends Command<TestSender> {
     }
 
     @Test
+    public void intArgument() {
+        final String input = "test hello 12";
+        final TestSender sender = new TestSender();
+        final Result result = this.execute(sender, input);
+        assert result.successful();
+        assert Objects.equals("int 12", sender.output) : sender.output;
+    }
+
+    @Test
+    public void pluralArgument() {
+        final String input = "test hello beans on toast";
+        final TestSender sender = new TestSender();
+        final Result result = this.execute(sender, input);
+        assert result.successful();
+        assert Objects.equals("beans on toast", sender.output) : sender.output;
+    }
+
+    @Test
     public void twoArgument() {
         final String input = "test hello there";
         final TestSender sender = new TestSender();
@@ -48,11 +71,26 @@ public class CommandTest extends Command<TestSender> {
         assert Objects.equals("beans", sender.output) : sender.output;
     }
 
+    @Test
+    public void testPatterns() {
+        assert Arrays.toString(this.patterns()).equals("[test, test hello, test hello there, test hello <int>, test hello <string>, test hello <string...>]") : Arrays.toString(this.patterns());
+    }
+
     @Override
     public Behaviour create() {
         return this.command("test")
             .arg("hello", (sender, arguments) -> {
                 sender.output = "hello";
+                return CommandResult.PASSED;
+            })
+            .arg("hello", INTEGER, (sender, arguments) -> {
+                assert !arguments.isEmpty();
+                sender.output = "int " + arguments.get(0);
+                return CommandResult.PASSED;
+            })
+            .arg("hello", STRING_END, (sender, arguments) -> {
+                assert !arguments.isEmpty();
+                sender.output = arguments.get(0);
                 return CommandResult.PASSED;
             })
             .arg("hello", String.class, (sender, arguments) -> {

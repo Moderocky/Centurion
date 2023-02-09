ae37fcb46b9158296cf2cfade7b1e4db32bbfe8f
Moderocky <admin@moderocky.com>
2023-02-09 11:41:45 +0000

    Add enum-based argument type.

---
+++ core/src/main/java/mx/kenzie/centurion/EnumArgument.java
@@ -0,0 +1,32 @@
+package mx.kenzie.centurion;
+
+public class EnumArgument<Type extends Enum<Type>> extends HashedArg<Type> {
+    public EnumArgument(Class<Type> type) {
+        super(type);
+    }
+
+    @Override
+    public boolean matches(String input) {
+        this.lastHash = input.hashCode();
+        try {
+            return this.parseNew(input) != null;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public Type parseNew(String input) {
+        return lastValue = Enum.valueOf(type, input.trim().toUpperCase());
+    }
+
+    @Override
+    public String[] possibilities() {
+        if (possibilities != null && possibilities.length > 0) return possibilities;
+        final Type[] enums = type.getEnumConstants();
+        this.possibilities = new String[enums.length];
+        for (int i = 0; i < enums.length; i++) possibilities[i] = enums[i].name();
+        return possibilities;
+    }
+
+}

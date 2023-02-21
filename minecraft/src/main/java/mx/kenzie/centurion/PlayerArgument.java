33f480edc8814d474826ca09743e98ec01621248
Moderocky <admin@moderocky.com>
2023-02-21 17:00:43 +0000

    Add player possibilities.

--- minecraft/src/main/java/mx/kenzie/centurion/PlayerArgument.java
+++ minecraft/src/main/java/mx/kenzie/centurion/PlayerArgument.java
@@ -3,6 +3,9 @@ package mx.kenzie.centurion;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class PlayerArgument extends TypedArgument<Player> {
     public PlayerArgument() {
         super(Player.class);
@@ -17,4 +20,13 @@ class PlayerArgument extends TypedArgument<Player> {
     public Player parse(String input) {
         return Bukkit.getPlayer(input);
     }
+
+    @Override
+    public String[] possibilities() {
+        if (possibilities.length > 0) return possibilities;
+        final List<String> list = new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) list.add(player.getName());
+        return list.toArray(new String[0]);
+    }
+
 }

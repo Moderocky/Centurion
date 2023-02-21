70bf188a5b67d2d36ea373c02287960e9c9f0ae4
Moderocky <admin@moderocky.com>
2023-02-21 17:00:39 +0000

    Add world possibilities.

--- minecraft/src/main/java/mx/kenzie/centurion/WorldArgument.java
+++ minecraft/src/main/java/mx/kenzie/centurion/WorldArgument.java
@@ -3,6 +3,9 @@ package mx.kenzie.centurion;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class WorldArgument extends TypedArgument<World> {
     public WorldArgument() {
         super(World.class);
@@ -17,4 +20,13 @@ class WorldArgument extends TypedArgument<World> {
     public World parse(String input) {
         return Bukkit.getWorld(input);
     }
+
+    @Override
+    public String[] possibilities() {
+        if (possibilities.length > 0) return possibilities;
+        final List<String> list = new ArrayList<>();
+        for (World world : Bukkit.getWorlds()) list.add(world.getName());
+        return list.toArray(new String[0]);
+    }
+
 }

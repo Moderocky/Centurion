c833d5a00e8713914a8abfbb9a5612cce9dd30f5
Moderocky <admin@moderocky.com>
2023-02-23 13:40:20 +0000

    Export argument container from command.

---
+++ core/src/main/java/mx/kenzie/centurion/ArgumentContainer.java
@@ -0,0 +1,105 @@
+package mx.kenzie.centurion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ArgumentContainer {
+    private final Argument<?>[] arguments;
+
+    ArgumentContainer(Argument<?>... arguments) {
+        this.arguments = arguments;
+    }
+
+    public boolean requireEmpty() {
+        return true;
+    }
+
+    public int weight() {
+        int weight = 0;
+        for (Argument<?> argument : arguments) weight += argument.weight();
+        return weight;
+    }
+
+    public int size() {
+        return arguments.length;
+    }
+
+    public boolean hasInput() {
+        for (Argument<?> argument : arguments) if (!argument.literal()) return true;
+        return false;
+    }
+
+    public boolean hasOptional() {
+        for (Argument<?> argument : arguments) if (argument.optional()) return true;
+        return false;
+    }
+
+    public Object[] check(String input, boolean passAllArguments) {
+        final Result result = this.consume(input, passAllArguments);
+        if (result == null) return null;
+        return result.inputs;
+    }
+
+    public Result consume(String initial, boolean passAllArguments) {
+        String input = initial;
+        final List<Object> inputs = new ArrayList<>(8);
+        for (Argument<?> argument : arguments) {
+            final Argument.ParseResult result = argument.read(input);
+            final String part = result.part(), remainder = result.remainder();
+            if (part.isEmpty() && argument.optional()) {
+                inputs.add(argument.lapse());
+                input = remainder;
+                continue;
+            }
+            if (!argument.matches(part)) return null;
+            if (!passAllArguments && argument.literal()) {
+                input = remainder;
+                continue;
+            }
+            inputs.add(argument.parse(part));
+            input = remainder;
+        }
+        if (this.requireEmpty() && !input.isBlank()) return null;
+        final String part = initial.substring(0, initial.length() - input.length()).trim();
+        final String remainder = input.stripLeading();
+        return new Result(part, remainder, inputs.toArray(new Object[0]));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        for (Argument<?> argument : arguments) {
+            builder.append(' ');
+            final boolean optional = argument.optional(), literal = argument.literal(), plural = argument.plural();
+            if (optional) builder.append('[');
+            else if (!literal) builder.append('<');
+            builder.append(argument.label());
+            if (plural) builder.append("...");
+            if (optional) builder.append(']');
+            else if (!literal) builder.append('>');
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArgumentContainer that)) return false;
+        return Arrays.equals(arguments, that.arguments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(arguments);
+    }
+
+    public Argument<?>[] arguments() {
+        return arguments;
+    }
+
+    protected record Result(String part, String remainder, Object... inputs) {
+    }
+
+
+}

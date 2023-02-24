be39be16ad5c8dc5060419e2ce9fcf38529962c4
Moderocky <admin@moderocky.com>
2023-02-24 12:38:40 +0000

    Add relative number class.

---
+++ minecraft/src/main/java/mx/kenzie/centurion/RelativeNumber.java
@@ -0,0 +1,40 @@
+package mx.kenzie.centurion;
+
+public class RelativeNumber extends Number {
+    private final Number number;
+    private final boolean relative;
+
+    public RelativeNumber(Number number, boolean relative) {
+        this.number = number;
+        this.relative = relative;
+    }
+
+    @Override
+    public int intValue() {
+        return number.intValue();
+    }
+
+    @Override
+    public long longValue() {
+        return number.longValue();
+    }
+
+    @Override
+    public float floatValue() {
+        return number.floatValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return number.doubleValue();
+    }
+
+    public boolean isRelative() {
+        return relative;
+    }
+
+    public Number getNumber() {
+        return number;
+    }
+
+}

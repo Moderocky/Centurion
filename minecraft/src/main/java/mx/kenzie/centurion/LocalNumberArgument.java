e06f16268289b48c8153513a19d0fd25fb2b139e
Moderocky <admin@moderocky.com>
2023-02-26 15:55:31 +0000

    Create local number parser.

---
+++ minecraft/src/main/java/mx/kenzie/centurion/LocalNumberArgument.java
@@ -0,0 +1,28 @@
+package mx.kenzie.centurion;
+
+public class LocalNumberArgument extends HashedArg<RelativeNumber> {
+
+    public LocalNumberArgument() {
+        super(RelativeNumber.class);
+        this.label = "number";
+    }
+
+    @Override
+    public boolean matches(String input) {
+        if (!input.startsWith("^")) return false;
+        try {
+            this.lastValue = this.parseNew(input);
+            this.lastHash = input.hashCode();
+            return true;
+        } catch (Throwable ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public RelativeNumber parseNew(String input) {
+        if (input.equals("^")) return new RelativeNumber(0, true);
+        return new RelativeNumber(Double.parseDouble(input.substring(1)), true);
+    }
+
+}

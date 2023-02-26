da077a25cc77f8a3a1fa7ac6bb081ea7b6e55490
Moderocky <admin@moderocky.com>
2023-02-26 15:55:15 +0000

    Add lapsed pass result.

--- core/src/main/java/mx/kenzie/centurion/CommandResult.java
+++ core/src/main/java/mx/kenzie/centurion/CommandResult.java
@@ -1,14 +1,27 @@
 package mx.kenzie.centurion;
 
 public enum CommandResult implements Result {
+    /**
+     * The command passed successfully, meeting expected conditions.
+     */
     PASSED(true, true),
+    /**
+     * No behaviour was met, the command fell through to an empty lapse function.
+     */
     NO_BEHAVIOUR(false, true),
     /**
      * This result is special - it falls back to the command parser rather than exiting as a failure.
      * This can be used to scrutinise inputs outside an argument matcher.
      */
     WRONG_INPUT(false, false),
+    /**
+     * This result is special - it causes the parser to fall straight to the `lapse` option.
+     */
     LAPSE(false, false),
+    /**
+     * The command passed successfully, but it hit the lapse criteria (whether by fault or by design.)
+     */
+    LAPSED(true, true),
     FAILED_UNKNOWN(false, true),
     FAILED_EXCEPTION(false, true);
     public final boolean successful, endParsing;

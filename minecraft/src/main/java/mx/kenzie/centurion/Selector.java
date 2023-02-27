7bc1452c071fdae88aa1ae18eeeafb6c485e1298
Moderocky <admin@moderocky.com>
2023-02-27 10:46:18 +0000

    Add selector verifier and wrapper.

---
+++ minecraft/src/main/java/mx/kenzie/centurion/Selector.java
@@ -0,0 +1,41 @@
+package mx.kenzie.centurion;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class Selector {
+
+    private final String selector;
+    private List<Entity> list;
+    private CommandSender sender;
+
+    protected Selector(String selector) {
+        this.selector = selector;
+    }
+
+    protected boolean verify() {
+        if (sender == null) sender = Bukkit.getConsoleSender();
+        try {
+            this.list = Bukkit.selectEntities(sender, selector);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    public Entity getEntity(CommandSender sender) {
+        final List<Entity> list = this.getEntities(sender);
+        if (list.size() > 0) return list.get(0);
+        return null;
+    }
+
+    public @NotNull List<Entity> getEntities(CommandSender sender) {
+        if (sender == this.sender && list != null) return list;
+        return list = Bukkit.selectEntities(this.sender = sender, selector);
+    }
+
+}

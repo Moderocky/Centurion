b4f5225e44703c16557538715ccd9852cca2977c
Moderocky <admin@moderocky.com>
2023-02-27 14:45:04 +0000

    Add text colour argument.

---
+++ minecraft/src/main/java/mx/kenzie/centurion/ColorArgument.java
@@ -0,0 +1,34 @@
+package mx.kenzie.centurion;
+
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+
+class ColorArgument extends HashedArg<TextColor> {
+    public ColorArgument() {
+        super(TextColor.class);
+    }
+
+    @Override
+    public boolean matches(String input) {
+        this.lastHash = input.hashCode();
+        this.lastValue = null;
+        try {
+            return this.parseNew(input) != null;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public TextColor parseNew(String input) {
+        final TextColor color = NamedTextColor.NAMES.value(input.toLowerCase());
+        return lastValue = color != null ? color : TextColor.fromHexString(input);
+    }
+
+    @Override
+    public String[] possibilities() {
+        if (possibilities.length > 0) return possibilities;
+        return NamedTextColor.NAMES.keys().toArray(new String[0]);
+    }
+
+}

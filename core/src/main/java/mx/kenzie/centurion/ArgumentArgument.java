9bf4614b3976ae34d546a15b246315b67dfd2128
Moderocky <admin@moderocky.com>
2023-03-01 17:11:56 +0000

    Add argument argument type.

---
+++ core/src/main/java/mx/kenzie/centurion/ArgumentArgument.java
@@ -0,0 +1,65 @@
+package mx.kenzie.centurion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgumentArgument extends TypedArgument<Argument> {
+
+    public ArgumentArgument() {
+        super(Argument.class);
+        this.label = "argument";
+    }
+
+    public static ArgumentArgument of(Argument<?>... arguments) {
+        return new ArgumentArgument() {
+            private Map<String, Argument<?>> map;
+
+            @Override
+            protected Map<String, Argument<?>> getArguments() {
+                if (map != null) return map;
+                this.map = new HashMap<>();
+                for (Argument<?> argument : arguments) map.put(argument.label(), argument);
+                return map;
+            }
+        };
+    }
+
+    public static ArgumentArgument of(Command<?> command) {
+        return new ArgumentArgument() {
+            private Map<String, Argument<?>> map;
+
+            @Override
+            protected Map<String, Argument<?>> getArguments() {
+                if (map != null) return map;
+                this.map = new HashMap<>();
+                for (ArgumentContainer argument : command.behaviour.arguments) this.addAll(argument, map);
+                return map;
+            }
+        };
+    }
+
+    @Override
+    public boolean matches(String input) {
+        return this.getArguments().containsKey(input);
+    }
+
+    @Override
+    public Argument<?> parse(String input) {
+        return this.getArguments().get(input);
+    }
+
+    protected Map<String, Argument<?>> getArguments() {
+        final Map<String, Argument<?>> map = new HashMap<>();
+        final Command<?>.Context context = Command.getContext();
+        if (context == null) return map;
+        final Command<?> command = Command.getContext().getCommand();
+        if (command == null) return map;
+        for (ArgumentContainer container : command.behaviour.arguments) this.addAll(container, map);
+        return map;
+    }
+
+    protected void addAll(ArgumentContainer container, Map<String, Argument<?>> map) {
+        for (Argument<?> argument : container.arguments) map.put(argument.label(), argument);
+    }
+
+}

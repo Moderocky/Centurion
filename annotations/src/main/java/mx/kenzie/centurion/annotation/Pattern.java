ca3752284c2a1578aefbc7eb0a8b1e762302304b
Moderocky <admin@moderocky.com>
2023-10-27 11:47:58 +0100

    Add pattern annotation.

---
+++ annotations/src/main/java/mx/kenzie/centurion/annotation/Pattern.java
@@ -0,0 +1,16 @@
+package mx.kenzie.centurion.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Pattern {
+
+    String value() default "";
+
+    String description() default "";
+
+}
